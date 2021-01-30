6471a7c522f435aed742537f92dbb1f6dd9a316f
rose-okoth <okoth.rose0@gmail.com>
2021-01-30 15:47:32 +0300

    bind the goal detail component with the goal component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,11 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
     {author:'William James', quote:'Act as if what you do makes a difference. IT DOES.', username:''},
-    {author:'William James', quote:'Success is not final, failure is not fatal: it is the COURAGE TO CONTINUE that counts.', username:''},
-    {author:'William James', quote:'What you get by achieving your goals is not as important as what you become by achieving your goals.', username:''},    
-    {author:'William James', quote:'Believe you can and you are halfway there.',username:''},
+    {author:'Winston Churchill', quote:'Success is not final, failure is not fatal: it is the COURAGE TO CONTINUE that counts.', username:''},
+    {author:'Zig Ziglar', quote:'What you get by achieving your goals is not as important as what you become by achieving your goals.', username:''},    
+    {author:'Theodore Roosevelt', quote:'Believe you can and you are halfway there.',username:''},
     {author:'William James', quote:'It is our attitude at the beginning of a difficult task which, ore than anything else will affect its successful outcome.', username:''},
-    {author:'William James', quote:'Life is like riding a bicycle. To keep your BALANCE, you must keep moving.', username:''},
+    {author:'Albert Einstein', quote:'Life is like riding a bicycle. To keep your BALANCE, you must keep moving.', username:''},
   ];
 
   constructor() { }
