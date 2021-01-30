79ffd31596dff12be6e0d4ee8c32778a869335ca
rose-okoth <okoth.rose0@gmail.com>
2021-01-30 15:18:25 +0300

    added in the quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  quotes:string[];
 
-  constructor(){
-    this.quotes = [
-      'Act as if what you do makes a difference. IT DOES.', 
-      'Success is not final, failure is not fatal: it is the COURAGE TO CONTINUE that counts.', 
-      'What you get by achieving your goals is not as important as what you become by achieving your goals.',
-      'Believe you can and you are halfway there.',
-      'It is our attitude at the beginning of a difficult task which, ore than anything else will affect its successful outcome.',
-      'Life is like riding a bicycle. To keep your BALANCE, you must keep moving.'
-    ]
-  } 
+export class AppComponent {
+  quotes:Quote[] = [
+    {author:'William James', quote:'Act as if what you do makes a difference. IT DOES.', username:''},
+    {author:'William James', quote:'Success is not final, failure is not fatal: it is the COURAGE TO CONTINUE that counts.', username:''},
+    {author:'William James', quote:'What you get by achieving your goals is not as important as what you become by achieving your goals.', username:''},    
+    {author:'William James', quote:'Believe you can and you are halfway there.',username:''},
+    {author:'William James', quote:'It is our attitude at the beginning of a difficult task which, ore than anything else will affect its successful outcome.', username:''},
+    {author:'William James', quote:'Life is like riding a bicycle. To keep your BALANCE, you must keep moving.', username:''},
+  ];
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,13 +1,13 @@
 export class Quote {
-    upvote: number;
-    downvote: number;
+    // upvote: number;
+    // downvote: number;
     
     constructor (
         public author: string,
         public quote: string,
         public username: string,
     ) {
-        this.upvote = 0;
-        this.downvote = 0;
+        // this.upvote = 0;
+        // this.downvote = 0;
     }
 }
